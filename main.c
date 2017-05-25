0a3b8deb2472433bc6bd3587e8c623faa81d5a1c
f1stm3d4ddy <skachkov1bazinga@gmail.com>
2017-05-25 22:36:16 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 /* main.c */
 #include "matr.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 void read_matr(double **m, int n)
 {
 /* implement the reading n*n numbers from stdin to 2-dim array */
@@ -8,13 +10,53 @@ void read_matr(double **m, int n)
 
 int main()
 {
-	int n;
-	double **a;
-	double **b;
+	int N;
+	int i, j;
+	scanf("%d", &N);
+	double **a = (double**)malloc(sizeof(double)*N);
+	double **b = (double**)malloc(sizeof(double)*N);
+	for (i = 0; i < N; i++)
+	{
+		a[i] = (double*)malloc(sizeof(double)*N);
+		b[i] = (double*)malloc(sizeof(double)*N);	
+	}
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			a[i][j] = rand() % 11;
+			b[j][i] = rand() % 11;
+		}
+	}
+	//output
+/*	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			printf("%lf ", a[i][j]);
+		}
+		printf("\n");
+	}
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			printf("%lf ", b[i][j]);
+		}
+		printf("\n");
+	}
+	*/
 /* main for tests: alloc mem for 2 square matrices and read them from stdin, then run 'matr()' */
 /* input format (stdin): n
  then 2 square matrices (n * n)
 */	
-	matr(a,b,n);
+	matr(a,b,N);
+	for (i = 0; i < N; i++)
+	{
+		free(a[i]);
+		free(b[i]);
+	}
+	free(a);
+	free(b);
 	return 0;
 }
--- matr.c
+++ matr.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "matr.h"
+#include <time.h>
 
 //Input: a - matrix (dim * dim), b - matrix (dim * dim) 
 //Output: none
@@ -9,17 +10,41 @@ void matr(double **a, double **b, int dim)
 {
 	//put your source code here
 	//
-	double **res;
+	double **c = (double**)malloc(sizeof(double)*dim);
 	int i = 0;
 	int j = 0;
-
-
-	for(;i<dim;i++)
+	int k = 0;
+	double s = 0;
+	for (i = 0; i < dim; i++)
 	{
-		for (;j<dim;j++)
-			printf("%lf ", res[i][j]);
+		c[i] = (double*)malloc(sizeof(double)*dim);
+	}
+	int start = clock();
+	for (i = 0; i < dim; i++)
+	{
+		for (j = 0; j < dim; j++)
+		{
+			for (k = 0; k < dim; k++)
+			{
+				s = s + a[i][k] * b[i][k];
+			}
+			c[i][j] = s;
+			s = 0;
+		}
+	}
+	int end = clock();
+	printf("%u", end - start);
+/*	for(i = 0; i < dim; i++)
+	{
+		for (j = 0; j < dim; j++)
+			printf("%lf ", c[i][j]);
 		printf("\n");
 	}
-
+*/
+	for (i = 0; i < dim; i++)
+	{
+		free(c[i]);
+	}
+	free(c);
 	return;
 }
